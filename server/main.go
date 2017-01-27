eb56cdd04ff83ca171fda005d7d4964819ecec41
[tj] <tj@enoti.me>
2017-01-27 16:21:45 +0000

    go fmt noise

--- server/main.go
+++ server/main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-  "net/http" 
-  "io"
-  "encoding/json"
-  "fmt"
-  "time"
-  "math/rand"
-  "crypto/sha1"
-  "encoding/base64"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"time"
 )
 
 type TokenCookie struct {
-  Host string `json:"host"`
-  Time string `json:"time"`
-  Token string  `json:"token"`
-  DSCP int `json:"dscp"`
+	Host  string `json:"host"`
+	Time  string `json:"time"`
+	Token string `json:"token"`
+	DSCP  int    `json:"dscp"`
 }
 
 func start(res http.ResponseWriter, req *http.Request) {
-  res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Content-Type", "application/json")
 
-  host := req.Header.Get("X-Real-IP")
-  time := time.Now().UTC().Format("20060102150405")
-  randval := string(rand.Intn(1000000))
+	host := req.Header.Get("X-Real-IP")
+	time := time.Now().UTC().Format("20060102150405")
+	randval := string(rand.Intn(1000000))
 
-  token := host + time + randval
+	token := host + time + randval
 
-  hash := sha1.New()
-  hash.Write([]byte(token))
-  sekret := base64.StdEncoding.EncodeToString(hash.Sum(nil))
+	hash := sha1.New()
+	hash.Write([]byte(token))
+	sekret := base64.StdEncoding.EncodeToString(hash.Sum(nil))
 
-  cookie := TokenCookie{Host: host, Time: time, Token:sekret}
-  fmt.Println(cookie)
-  json.NewEncoder(res).Encode(cookie)
+	cookie := TokenCookie{Host: host, Time: time, Token: sekret}
+	fmt.Println(cookie)
+	json.NewEncoder(res).Encode(cookie)
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
-  res.Header().Set(
-    "Content-Type",
-    "text/html",
-  )
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
 
-  io.WriteString(
-    res,
-    `<DOCTYPE html>
+	io.WriteString(
+		res,
+		`<DOCTYPE html>
 <html>
   <head>
       <title>Hello World</title>
@@ -63,13 +63,12 @@ func index(res http.ResponseWriter, req *http.Request) {
     </p>
   </body>
 </html>`,
-  )
+	)
 }
 func main() {
-  http.HandleFunc("/", index)
-  http.HandleFunc("/start", start)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/start", start)
 
-
-  fmt.Println("Starting up Server, listening on port: 4000")
-  http.ListenAndServe(":4000", nil)
+	fmt.Println("Starting up Server, listening on port: 4000")
+	http.ListenAndServe(":4000", nil)
 }
